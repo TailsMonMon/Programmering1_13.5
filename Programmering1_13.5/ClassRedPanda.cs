06a8996aaa365318205401f44d86507fb2eac5b6
Monica Björk <m.bjork95@gmail.com>
2018-05-11 18:37:39 +0200

    Add project files.

---
+++ Programmering1_13.5/ClassRedPanda.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_13._5 {
+    class ClassRedPanda {
+        //      SAME FOR ALL
+        private static int eyes = 2;
+        private static int legs = 4;
+        private static int tail = 1;
+
+        public ClassRedPanda(bool gender) {
+            if(gender == true) {
+                Console.WriteLine("It's a female!");
+            }
+            else {
+                Console.WriteLine("It's a male!");
+            }
+        }
+
+        //      UNIQUE FOR ALL
+        private string name;
+        public string Name {
+            get {
+                return name;
+            }
+            set {
+                name = value;
+            }
+        }
+
+        private int age;
+        public int Age {
+            get {
+                return age;
+            }
+            set {
+                age = value;
+            }
+        }
+
+        //      STUFF THEY DO
+        public static void Sleep(string name) {    // Need to mention "name" as an agument here since it's private.
+            Console.WriteLine("sssh, {0} sover", name);
+            return;
+        }
+    }
+}
---
+++ Programmering1_13.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_13._5 {
+    class Program {
+        static void Main(string[] args) {
+            ClassRedPanda redPanda = new ClassRedPanda(true);
+            redPanda.Name = "Tails";
+            redPanda.Age = 23;
+
+            Console.WriteLine("Denna kattbjörnen heter {0} och är {1}år gammal.", redPanda.Name, redPanda.Age);
+            Console.ReadKey();
+
+            ClassRedPanda.Sleep(redPanda.Name);
+            Console.ReadKey();
+        }
+    }
+}
